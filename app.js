1be0e38b945b2975920a106ab8491430fc689819
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-17 23:42:45 -0700

    Added Q 6, still needs work + Q7

--- app.js
+++ app.js
@@ -1,67 +1,162 @@
 'use strict';
 
-alert('Welcome to my page! Here you can take a little quiz to see how well you know me. Click "OK" to continue and try it out!');
+var userGreet = prompt('Hello, what\'s your name?');
+var userName = userGreet.charAt(0).toUpperCase() + userGreet.slice(1).toLowerCase();
+console.log('User is:', userName);
+alert('Welcome to my page, ' + userName + '! Here you can take a little quiz to see how well you know me. Click "OK" to continue and try it out!');
 
-var musicQuestionAnswer = prompt ('Do I play an instrument?').toLowerCase();
+var numCorrect = 0;
 
-if (musicQuestionAnswer === 'yes' || musicQuestionAnswer === 'y') {
-  alert('I wish! I failed to learn how to play any despite taking lessons in violin, guitar, and piano.');
-} else if (musicQuestionAnswer === 'no' || musicQuestionAnswer === 'n') {
+var musicQuestionAnswer = prompt('Do I play an instrument?').toLowerCase();
+var musicCorrect = (musicQuestionAnswer === 'no' || musicQuestionAnswer === 'n');
+var musicIncorrect = (musicQuestionAnswer === 'yes' || musicQuestionAnswer === 'y');
+
+if (musicCorrect) {
   alert('Unfortunately, that\'s right.');
+  console.log('User response: correct');
+} else if (musicIncorrect) {
+  alert('I wish! I failed to learn how to play any despite taking lessons in violin, guitar, and piano.');
+  console.log('User response: incorrect');
 } else {
   alert('Try again with a simple "yes" or "no" reponse - if you don\'t know, just guess!');
+  console.log('User response: invalid');
 }
 
-var pilotQuestionAnswer = prompt ('Have I ever flone an airplane?').toLowerCase();
+if (musicCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
 
-if (pilotQuestionAnswer === 'yes' || pilotQuestionAnswer === 'y') {
+var pilotQuestionAnswer = prompt('Have I ever flown an airplane?').toLowerCase();
+var pilotCorrect = (pilotQuestionAnswer === 'yes' || pilotQuestionAnswer === 'y');
+var pilotIncorrect = (pilotQuestionAnswer === 'no' || pilotQuestionAnswer === 'n');
+
+if (pilotCorrect) {
   alert('Yes, I have! It\'s been about ten years though, so I\'m not sure how much I would remember now.');
-} else if (pilotQuestionAnswer === 'no' || pilotQuestionAnswer === 'n') {
+  console.log('User response: correct');
+} else if (pilotIncorrect) {
   alert('I flew solo when I was 18, actually! My family is really into aviation.');
+  console.log('User response: incorrect');
 } else {
   alert('Try again with a simple "yes" or "no" reponse - if you don\'t know, just guess!');
+  console.log('User response: invalid');
 }
 
-var petQuestionAnswer = prompt ('Do I have any pets?').toLowerCase();
+if (pilotCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
 
-if (petQuestionAnswer === 'yes' || petQuestionAnswer === 'y') {
+var petQuestionAnswer = prompt('Do I have any pets?').toLowerCase();
+var petCorrect = (petQuestionAnswer === 'yes' || petQuestionAnswer === 'y');
+var petIncorrect = (petQuestionAnswer === 'no' || petQuestionAnswer === 'n');
+
+if (petCorrect) {
   alert('You\'re right! I have a very spoiled Texas Heeler named Remy.');
-} else if (petQuestionAnswer === 'no' || petQuestionAnswer === 'n') {
+  console.log('User response: correct');
+} else if (petIncorrect) {
   alert('I\'m actually very much a dog owner - take a look inside my car and you\'d know immediately!');
+  console.log('User response: incorrect');
 } else {
   alert('Try again with a simple "yes" or "no" reponse - if you don\'t know, just guess!');
+  console.log('User response: invalid');
 }
 
-var childrenQuestionAnswer = prompt ('Do I have any children?').toLowerCase();
+if (petCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
 
-if (childrenQuestionAnswer === 'yes' || childrenQuestionAnswer === 'y') {
-  alert('Unless you count fur-children, no motherhood for me!');
-} else if (childrenQuestionAnswer === 'no' || childrenQuestionAnswer === 'n') {
+var childrenQuestionAnswer = prompt('Do I have any children?').toLowerCase();
+var childrenCorrect = (childrenQuestionAnswer === 'no' || childrenQuestionAnswer === 'n');
+var childrenIncorrect = (childrenQuestionAnswer === 'yes' || childrenQuestionAnswer === 'y');
+
+if (childrenCorrect) {
   alert('Nailed it!');
+  console.log('User response: correct');
+} else if (childrenIncorrect) {
+  alert('Unless you count fur-children, no motherhood for me!');
+  console.log('User response: incorrect');
 } else {
   alert('Try again with a simple "yes" or "no" reponse - if you don\'t know, just guess!');
+  console.log('User response: invalid');
 }
 
-var athleticQuestionAnswer = prompt ('Do I enjoy any athletic activities?').toLowerCase();
+if (childrenCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
 
-if (athleticQuestionAnswer === 'yes' || athleticQuestionAnswer === 'y') {
-  alert('Of course I do - skiing, hiking, and paddleboarding are my favorites!');
-} else if (athleticQuestionAnswer === 'no' || athleticQuestionAnswer === 'n') {
+var athleticQuestionAnswer = prompt('Do I enjoy any athletic activities?').toLowerCase();
+var athleticCorrect = (athleticQuestionAnswer === 'yes' || athleticQuestionAnswer === 'y');
+var athleticIncorrect = (athleticQuestionAnswer === 'no' || athleticQuestionAnswer === 'n');
+
+if (athleticCorrect) {
+  alert('Of course I do! Skiing, hiking, and paddleboarding are my favorites.');
+  console.log('User response: correct');
+} else if (athleticIncorrect) {
   alert('How dare you, I\'m not that out of shape!');
+  console.log('User response: incorrect');
 } else {
   alert('Try again with a simple "yes" or "no" reponse - if you don\'t know, just guess!');
+  console.log('User response: invalid');
 }
 
-// var randomNum = Math.floor(Math.random() * 11);
-// console.log('the number is:', randomNum);
+if (athleticCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
+
+alert('Okay, ' + userName + ', so far you\'ve gotten ' + numCorrect + ' facts correct, but now I want you to try to read my mind. Click OK to give it a shot!');
+
+var randomNum = Math.floor(Math.random() * 11);
+console.log('The number is:', randomNum);
+
+var userGuess = prompt('I\'m thinking of a number between 0-10 and you will have a total of four attempts to get it correct. What do you think it is?');
+userGuess = Number(userGuess);
+console.log('User guess is:', userGuess, typeof userGuess, typeof randomNum);
+
+
+for (var i = 0; i < 4; i++) {
+  console.log(i);
+  if (i = 4) break;
+}
+
+var turns = i;
+console.log('Number of turns:', turns);
+var guessCorrect = (userGuess === randomNum);
+
+while (i <= 4) {
+  if (guessCorrect) {
+    alert('You got it, you must have psychic powers!');
+    turns = 0;
+    console.log(guessCorrect);
+    break;
+  } else if (userGuess > randomNum) {
+    prompt('Your number is too big! You have ' + turns + ' attempts left.');
+    turns = turns - 1;
+    console.log('Turns remaining:', turns);
+    break;
+  } else if (userGuess < randomNum) {
+    prompt('Your number is too small! You have ' + turns + ' attempts left.');
+    turns = turns - 1;
+    console.log('Turns remaining:', turns);
+    break;
+  }
+}
+
+if (guessCorrect) {
+  numCorrect++;
+  console.log('Number correct:', numCorrect);
+}
 //
-// for (var i = 0; i < 4; i++) {
-//   var userGuess = prompt('Guess a number from 0-10.');
-//   userGuess = Number(userGuess);
-//   console.log('User guess is:', userGuess, typeof userGuess, typeof randomNum);
-//   if (randomNum === userGuess) {
-//     alert('You got it!');
-//   } else {
-//     alert('Nope, try again!');
-//   }
-// }
+//   var statesQuestionAnswer = prompt('Okay, one last question: I currently live in Oregon, but which other states have I also lived in?');
+//   var statesCorrect = [Florida, Alaska];
+
+if (numCorrect === 7) {
+  alert('All ' + numCorrect + ' correct, nicely done, ' + userName + '! You must know me very well, or maybe you are just an excellent guesser!');
+} else if (numCorrect >= 3) {
+  alert(numCorrect + ' correct out of 7 isn\'t too bad, ' + userName + ', but maybe we should get to know each other a little better!');
+} else {
+  alert(numCorrect + ' correct out of 7 is pretty bad, ' + userName + '! You must not know me at all!');
+}
